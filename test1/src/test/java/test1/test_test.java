3072b5510deb8d0f3e7f67bfb3d7640a7cd9f120
mainaccount <mainaccount@HP.Home>
2017-10-01 22:06:38 +0100

    sample

---
+++ test1/src/test/java/test1/test_test.java
@@ -0,0 +1,17 @@
+package test1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class test_test {
+
+  
+	@Before
+	public void tes1() {
+		
+	}
+	@Test
+    public void testCounty() {
+		
+	}
+}
